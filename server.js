85edd3ecdb46c305cda49785848cd24b30fab84e
rushikesh <rushikesh@girdmail.com>
2025-04-19 16:36:08 +0530

    added server changes 19-04-25 cors

--- server.js
+++ server.js
@@ -12,12 +12,19 @@ dotenv.config();
 const app = express();
 app.use('/uploads/resumes', express.static('uploads/resumes'));
 
-app.use(cors(
-  {
-    origin:["http://localhost:4200","https://jobtech.netlify.app/"],
-    credentials:true,
-  }
-))
+const allowedOrigins = ["http://localhost:4200", "https://jobtech.netlify.app"];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
